30b35fb816ee1dfe7af9bbf75e8edfaa9bb8e912
bytedown <igor.yuzovitskiy@gmail.com>
2015-11-03 22:25:05 -0500

    Add background image gradient.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+  
+  def open_pdf
+    send_file(Rails.root.join("app", "assets", "pdfs", "customer_comments.pdf").to_s, disposition: "inline", type: "application/pdf")
+  end
 end
