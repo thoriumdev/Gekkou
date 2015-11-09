6bed0dfb746b4d434c302a217f139f3b001ae667
bytedown <igor.yuzovitskiy@gmail.com>
2015-11-09 11:39:51 -0500

    Setup temp site.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,10 @@ class WelcomeController < ApplicationController
   def index
   end
   
+  def temp_site
+    
+  end
+  
   def open_pdf
     send_file(Rails.root.join("app", "assets", "pdfs", "customer_comments.pdf").to_s, disposition: "inline", type: "application/pdf")
   end
